2e8ef373e10b64853b8308a0fc842e70da7e67b2
SimbaWANGU <simbawangulu@gmail.com>
2022-10-18 13:16:38 +0300

    setup project schema

--- db/migrate/20221018100559_create_author.rb
+++ db/migrate/20221018100559_create_author.rb
@@ -1,7 +1,10 @@
-class CreateAuthor < ActiveRecord::Migration[7.0]
+class CreateUser < ActiveRecord::Migration[7.0]
   def change
-    create_table :authors do |t|
-
+    create_table :user do |t|
+      t.string :name
+      t.text :photo
+      t.text :bio
+      t.integer :posts_counter
       t.timestamps
     end
   end
