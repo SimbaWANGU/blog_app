58123c3af17e3d0361ca0e1498ff72b48213cb22
SimbaWANGU <simbawangulu@gmail.com>
2022-11-03 11:00:30 +0300

    add current_user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
 class ApplicationController < ActionController::Base
+  add_flash_types :danger, :info, :warning, :success, :messages
+
+  def current_user
+    @current_user = Author.first
+  end
 end
