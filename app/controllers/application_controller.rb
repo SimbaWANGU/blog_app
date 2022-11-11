2fbaa1522796620ff164de48906590972e8ddada
SimbaWANGU <simbawangulu@gmail.com>
2022-11-11 13:44:06 +0300

    fix linters

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,26 +1,13 @@
 class ApplicationController < ActionController::Base
   add_flash_types :danger, :info, :warning, :success, :messages
-<<<<<<< HEAD
-
   protect_from_forgery with: :exception
-
   before_action :configure_permitted_parameters, if: :devise_controller?
-
   def logging_out; end
 
   protected
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :email, :password) }
-
-=======
-  protect_from_forgery with: :exception
-  before_action :configure_permitted_parameters, if: :devise_controller?
-  def logging_out; end
-  protected
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :email, :password) }
->>>>>>> 068c59cc5fc4d3ce534dcfbc331bf2b47809667b
     devise_parameter_sanitizer.permit(:account_update) do |u|
       u.permit(:name, :email, :password, :current_password)
     end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,14 +3,17 @@ class PostsController < ApplicationController
     @author = Author.find(params[:author_id])
     @posts = @author.posts.includes(:comments)
   end
+
   def show
     @author = Author.find(params[:author_id])
     @post = @author.posts.find(params[:id])
   end
+
   def new
     @id = current_author.id
     @new_post = Post.new
   end
+
   def create
     post = Post.new(title: params[:title], text: params[:text], author: current_author, comments_counter: 0,
                     likes_counter: 0)
